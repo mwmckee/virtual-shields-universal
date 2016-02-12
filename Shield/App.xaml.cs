192f15fe40752a954aef1595aa413c6b6e80f8ea
Matthew McKee <matt_mckee@outlook.com>
2016-02-11 18:23:33 -0800

    fix compatibility issues with latest WACK version

--- Shield/App.xaml.cs
+++ Shield/App.xaml.cs
@@ -131,6 +131,11 @@ namespace Shield
         /// <param name="e">Details about the launch request and process.</param>
         protected override async void OnLaunched(LaunchActivatedEventArgs e)
         {
+            if (e.PrelaunchActivated)
+            {
+                return;
+            }
+
 #if DEBUG
             if (Debugger.IsAttached)
             {
@@ -138,7 +143,6 @@ namespace Shield
             }
 
 #endif
-
             var rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
